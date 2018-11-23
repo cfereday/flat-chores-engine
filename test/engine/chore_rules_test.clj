87ff92d1e68d05ad597fbc4f94f747746528583b
Charlotte Fereday <cfereday@thoughtworks.com>
2018-11-23 15:16:47 +0000

    Charlotte - adding in more reports

--- test/engine/chore_rules_test.clj
+++ test/engine/chore_rules_test.clj
@@ -4,10 +4,10 @@
             [clara.rules :refer :all]
             [clara.tools.inspect :refer :all]))
 
-(deftest checks-chores
+(deftest checks-chores-for-individual-flatmates
   (testing "If a flatmate has completed a chore their chore status is marked as completed"
     (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport :vacuum "Charlotte" nil nil false))
+                    (insert (->FlatMateReport :vacuum "Charlotte" nil nil nil))
                     (fire-rules))
           query (query session chore-outcomes)
           mapped-query (into {} query)]
@@ -19,7 +19,7 @@
 
   (testing "If a flatmate hired a cleaner to complete their chore, their chore status is completed"
     (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport nil "Charlotte" :vacuum nil nil))
+                    (insert (->FlatMateReport nil "Charlotte" :vacuum nil nil))
                     (fire-rules))
           query (query session chore-outcomes)
           mapped-query (into {} query)]
@@ -30,7 +30,7 @@
 
   (testing "If another flatmate has done the task for the flatmate their chore status is completed"
     (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport nil "Charlotte" nil :vacuum nil))
+                    (insert (->FlatMateReport nil "Charlotte" nil :vacuum nil))
                     (fire-rules))
           query (query session chore-outcomes)
           mapped-query (into {} query)]
@@ -41,7 +41,7 @@
 
   (testing "If a flatmate was ill their chore status is marked as exempt and their illness is down in lieu of a chore"
     (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport nil "Charlotte" nil nil "flu"))
+                    (insert (->FlatMateReport nil "Charlotte" nil nil "flu"))
                     (fire-rules))
           query (query session chore-outcomes)
           mapped-query (into {} query)]
@@ -52,7 +52,7 @@
 
   (testing "If a flatmate has not completed a chore & wasn't ill their chore status is marked as incomplete"
     (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport nil "Charlotte" nil nil nil))
+                    (insert (->FlatMateReport nil "Charlotte" nil nil nil))
                     (fire-rules))
           query (query session chore-outcomes)
           mapped-query (into {} query)]
@@ -61,10 +61,48 @@
             (= (:?chore mapped-query) :missing))))))
 
 
-(deftest checks-chore-outcomes
+(deftest checks-chores-for-whole-flat
+    (let [session (-> (mk-session 'engine.chore-rules)
+                    (insert (->WeeklyReport
+                              {:chore-type                        :vacuum
+                               :flatmate-name                     "Charlotte"
+                               :chore-completed-by-cleaner        nil
+                               :chore-completed-by-other-flatmate nil
+                               :flatmate-ill                      nil}
+                              #_{:chore-type                        :kitchen
+                               :flatmate-name                     "Felipe"
+                               :chore-completed-by-cleaner        nil
+                               :chore-completed-by-other-flatmate nil
+                               :flatmate-ill                      nil}
+                              #_{:chore-type                        :bathroom
+                               :flatmate-name                     "Christoph"
+                               :chore-completed-by-cleaner        nil
+                               :chore-completed-by-other-flatmate nil
+                               :flatmate-ill                      nil}))
+                    (fire-rules))
+          flatmate-report-query (query session flatmate-reports)
+          all-records-report-query (query session all-records)
+          mapped-query (into {} flatmate-report-query)
+          mapped-query-all-records (into {} all-records-report-query)]
+
+      (prn "HERE IS ALL RECORDS query" all-records)
+      (prn "HERE IS FLATMATE REPORT query" flatmate-report-query)
+      (prn "HERE IS MAPPED query" mapped-query)
+      (prn "HERE IS ALL RECORDS MAPPED query" mapped-query-all-records)
+      (prn "HERE IS TRYIONG TO GET A KEY" (:?all-records mapped-query-all-records))
+      (prn "inspected session" (clojure.pprint/pprint (:insertions (inspect session))))
+      (is (= (some? mapped-query-all-records))
+
+
+        #_(and
+            (= (:?chore-type mapped-query) :kitchen)
+            (= (:?flatmate-name mapped-query) "Felipe")))))
+
+
+(deftest checks-movie-picker
   (testing "If a flatmate completed their chore they are able to pick the sunday movie"
     (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport :vacuum "Charlotte" nil nil nil))
+                    (insert (->FlatMateReport :vacuum "Charlotte" nil nil nil))
                     (fire-rules))
           query (query session can-choose-a-movie?)
           mapped-query (into {} query)]
@@ -74,17 +112,15 @@
 
   (testing "If a flatmate didn't complete their chore or are exempt they aren't able to pick the sunday movie"
     (let [incomplete-session (-> (mk-session 'engine.chore-rules)
-                    (insert (->WeeklyReport nil "Charlotte" nil nil nil))
+                    (insert (->FlatMateReport nil "Charlotte" nil nil nil))
                     (fire-rules))
           exempt-session (-> (mk-session 'engine.chore-rules)
-                           (insert (->WeeklyReport nil "Charlotte" nil nil "flu"))
+                           (insert (->FlatMateReport nil "Charlotte" nil nil "flu"))
                            (fire-rules))
           incomplete-query (query incomplete-session can-choose-a-movie?)
           exempt-query     (query exempt-session can-choose-a-movie?)
           mapped-incomplete-query (into {} incomplete-query)
           mapped-exempt-query (into {} exempt-query)]
-      (prn "HERE IS query" query)
-      (prn "inspected" (clojure.pprint/pprint (:insertions (inspect incomplete-session))))
       (is (and
             (= (:?eligibility mapped-incomplete-query) :no-movie-picking)
             (= (:?flatmate-name mapped-incomplete-query) "Charlotte")))
