3b5850f0fb1cf20eaca218ebe7ced5134ad93b54
Felipe Sere <felipesere@gmail.com>
2018-11-24 18:03:39 +0000

    Collate data into blog

--- test/engine/chore_rules_test.clj
+++ test/engine/chore_rules_test.clj
@@ -2,13 +2,14 @@
   (:require [clojure.test :refer :all]
             [engine.chore-rules :refer :all]
             [clara.rules :refer :all]
-            [clara.tools.inspect :refer :all]))
+            [clara.tools.inspect :refer :all]
+            [clojure.pprint :refer :all]))
 
 (defn person-facts
   [person]
   (let [flatmate (make-flatmate person)
         chores (map #(->Chore (:name person) %) (:chores person))]
-    (list flatmate chores)
+    (conj chores flatmate)
     ))
 
 (defn derive-facts-from
@@ -26,40 +27,26 @@
 
 (deftest felipes-test-suite
   (testing "That a sick flatname is automagically exempted"
-    (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (->FlatMate "Charlotte" true false))
-                    (fire-rules))
+    (let [facts (derive-facts-from [{:name "Charlotte" :was-ill true}])
+          session (create-session-with facts)
           [{result :?exemption}] (query session exemptions-query?)]
       (is (= result (->Exemption "Charlotte")))))
 
    (testing "doing 4 chores gives you beer"
-     (let [session (-> (mk-session 'engine.chore-rules)
-                     (insert (make-flatmate {:name "Charlotte"}))
-                     (insert (->Chore "Charlotte" :bathroom))
-                     (insert (->Chore "Charlotte" :kitchen))
-                     (insert (->Chore "Charlotte" :laundry))
-                     (insert (->Chore "Charlotte" :bins))
-                     (fire-rules))
+     (let [facts (derive-facts-from [{:name "Charlotte" :chores [:bathroom :kitchen :laundry :bins]}])
+           session (create-session-with facts)
            [{result :?beer}] (query session beer?)]
        (is (= result (->Beer "Charlotte")))))
 
   (testing "getting the cleaners"
-    (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (make-flatmate {:name "Charlotte" :paid-cleaner true}))
-                    (insert (->Chore "Charlotte" :bins))
-                    (fire-rules))
+    (let [facts (derive-facts-from [{:name "Charlotte" :paid-cleaner true :chores [:bin]}])
+          session (create-session-with facts)
           [{result :?beer}] (query session beer?)]
       (is (= result (->Beer "Charlotte")))))
 
   (testing "sadly being ill"
-    (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (make-flatmate {:name "Charlotte" :was-ill true}))
-                    (insert (->Chore "Charlotte" :bins))
-                    (insert (->Chore "Charlotte" :kitchen))
-                    (insert (->Chore "Charlotte" :cooking))
-                    (insert (->Chore "Charlotte" :bathroom))
-                    (insert (->Chore "Charlotte" :balcony))
-                    (fire-rules))
+    (let [facts (derive-facts-from [{:name "Charlotte" :was-ill true :chores [:bins :kitchen :cooking :bathroom :balcony]}])
+          session (create-session-with facts)
           result (query session beer?)]
       (is (empty? result ))))
   
@@ -70,14 +57,7 @@
 ;;      (has-beer? session))) ;; ---> runs my query AND removes Clara'isms from it... aka ?foo should be gone!
 ;;
   (testing "multiple flatmates"
-    (let [session (-> (mk-session 'engine.chore-rules)
-                    (insert (make-flatmate {:name "Felipe" :paid-cleaner true}))
-                    (insert (->Chore "Felipe" :bills))
-                    (insert (make-flatmate {:name "Charlotte"}))
-                    (insert (->Chore "Charlotte" :bins))
-                    (insert (->Chore "Charlotte" :dishes))
-                    (insert (->Chore "Charlotte" :bedroom))
-                    (insert (->Chore "Charlotte" :kitchen))
-                    (fire-rules))
+    (let [facts (derive-facts-from [{:name "Felipe" :paid-cleaner true :chores [:bills]} {:name "Charlotte" :chores [:bins :dishes :bedroom :kitchen]}])
+          session (create-session-with facts)
           result (query session beer?)]
       (is (= (beers result) (list (->Beer "Felipe") (->Beer "Charlotte")) )))))
