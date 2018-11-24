3e6d16eda97962987c6233d7887fd5a239f3d077
Felipe Sere <felipesere@gmail.com>
2018-11-24 17:14:50 +0000

    Move helper functions to the top

--- test/engine/chore_rules_test.clj
+++ test/engine/chore_rules_test.clj
@@ -4,6 +4,26 @@
             [clara.rules :refer :all]
             [clara.tools.inspect :refer :all]))
 
+(defn person-facts
+  [person]
+  (let [flatmate (make-flatmate person)
+        chores (map #(->Chore (:name person) %) (:chores person))]
+    (list flatmate chores)
+    ))
+
+(defn derive-facts-from
+  [people]
+  (mapcat person-facts people))
+
+(defn create-session-with 
+  [facts]
+  (let [session (mk-session 'engine.chore-rules)
+        with-facts (reduce insert session facts)]
+    (fire-rules with-facts)))
+
+(defn beers [result]
+  (map :?beer result))
+
 (deftest felipes-test-suite
   (testing "That a sick flatname is automagically exempted"
     (let [session (-> (mk-session 'engine.chore-rules)
@@ -12,7 +32,6 @@
           [{result :?exemption}] (query session exemptions-query?)]
       (is (= result (->Exemption "Charlotte")))))
 
-  ;; this would need to be refactored into a blog that gets taken aparat, I guess?
    (testing "doing 4 chores gives you beer"
      (let [session (-> (mk-session 'engine.chore-rules)
                      (insert (make-flatmate {:name "Charlotte"}))
@@ -21,19 +40,15 @@
                      (insert (->Chore "Charlotte" :laundry))
                      (insert (->Chore "Charlotte" :bins))
                      (fire-rules))
-           [{result :?beer}] (query session beer?)
-           ]
+           [{result :?beer}] (query session beer?)]
        (is (= result (->Beer "Charlotte")))))
 
-  ;; this would need to be refactored into a blog that gets taken aparat, I guess?
   (testing "getting the cleaners"
     (let [session (-> (mk-session 'engine.chore-rules)
                     (insert (make-flatmate {:name "Charlotte" :paid-cleaner true}))
                     (insert (->Chore "Charlotte" :bins))
                     (fire-rules))
-          [{result :?beer}] (query session beer?)
-          ;; _x (explain-activations session)]
-          ]
+          [{result :?beer}] (query session beer?)]
       (is (= result (->Beer "Charlotte")))))
 
   (testing "sadly being ill"
@@ -45,9 +60,7 @@
                     (insert (->Chore "Charlotte" :bathroom))
                     (insert (->Chore "Charlotte" :balcony))
                     (fire-rules))
-          result (query session beer?)
-          ;; _x (explain-activations session)]
-          ]
+          result (query session beer?)]
       (is (empty? result ))))
   
 
@@ -56,29 +69,6 @@
 ;;          session (create-session-with known-facts)] ;; ---> inserts said facts into Clara and runs fires the rules
 ;;      (has-beer? session))) ;; ---> runs my query AND removes Clara'isms from it... aka ?foo should be gone!
 ;;
-;;  (defn person-facts
-;;    [person]
-;;    (let [flatmate (make-flatmate person)
-;;          chores (map #(Chore. (:name person) %) (:chores person))]
-;;      '(flatmate chores)
-;;      ))
-;;
-;;  (defn derive-facts-from
-;;    [people]
-;;    (mapcat person-facts people))
-;;
-;;  (defn create-session-with 
-;;    [facts]
-;;    (let [session (mk-session 'engine.chore-rules)
-;;          with-facts (reduce insert session facts)]
-;;          (fire-rules with-facts)))
-
-
-  (defn beers [result]
-    (map :?beer result))
-
-
-
   (testing "multiple flatmates"
     (let [session (-> (mk-session 'engine.chore-rules)
                     (insert (make-flatmate {:name "Felipe" :paid-cleaner true}))
