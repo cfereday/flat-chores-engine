a6bbfb8b5b41f98b39e2ae173710e9b0b412d36e
Charlotte Fereday <cfereday@thoughtworks.com>
2018-11-24 21:27:06 +0000

    Charlotte - If a flatmate has completed all of their chores then they are eligible to get a free beer

--- src/engine/weekly_chores_if_else.clj
+++ src/engine/weekly_chores_if_else.clj
@@ -1,42 +1,23 @@
 (ns engine.weekly-chores-if-else)
 
+(def valid-chores
+  {:bathroom true
+   :kitchen  true
+   :laundry  true
+   :bins true})
+
+(defn valid-chore?
+  [chore]
+    (chore valid-chores))
 
 
 (defn chores-checker
   [weekly-flat-report]
-  (cond (= (count (:chores weekly-flat-report)) 4)
-        (assoc weekly-flat-report :status :completed)
-        :else (assoc weekly-flat-report :status :incomplete)))
+  (cond (and (= (count (:chores weekly-flat-report)) 4) (map (:chores weekly-flat-report) valid-chore?))
+        (assoc weekly-flat-report :status :completed :gets-beer true)
 
+        #_(= (:cleaner-hired weekly-flat-report) true)
+        #_(assoc weekly-flat-report :chores [:bathroom :kitchen :bins])
 
-
-
-(comment
-  (defn chores-checker
-    [chores-information]
-    (cond (some? (:chore-type chores-information))
-          (assoc chores-information :chore-status :completed)
-
-          (some? (:cleaner-hired chores-information))
-          (dissoc (assoc (assoc chores-information :chore-status :completed) :chore-type (:cleaner-hired (assoc chores-information :chore-status :completed))) :cleaner-hired)
-
-          (some? (:flate-mate-completed chores-information))
-          (dissoc (assoc (assoc chores-information :chore-status :completed) :chore-type (:flate-mate-completed (assoc chores-information :chore-status :completed))) :flate-mate-completed)
-
-          (some? (:ill chores-information))
-          (dissoc (assoc chores-information :chore-status :exempt) :ill)
-
-          :else (assoc chores-information :chore-status :incomplete)))
-
-  (defn outcome-checker
-    [chores-checker-outcome]
-    (cond (= (:chore-status chores-checker-outcome) :completed)
-          (prn "Congrats you did your chores, as a prize you can choose the flat sunday night movie!")
-
-          (= (:chore-status chores-checker-outcome) :exempt)
-          (prn "Sorry you were ill, get better soon")
-
-          (= (:chore-status chores-checker-outcome) :incomplete)
-          (prn "Oh no you didn't do your chores, please buy your flatmates pizza for the flat sunday night movie!"))))
-
+        :else (assoc weekly-flat-report :status :incomplete :gets-beer false)))
 
--- test/engine/weekly_chores_if_else_test.clj
+++ test/engine/weekly_chores_if_else_test.clj
@@ -10,40 +10,18 @@
   (testing "If a flatmate has completed 4 chore their chore status is marked as incomplete"
     (is (= (:status (chores-checker {:chores [:bathroom] :name "Charlotte"}) :incomplete))))
 
-  #_(testing "If you've completed all your chores you'll get a beer"
-    (is (= (chores-checker )))
+  (testing "If you've completed all your chores you'll get a beer"
+    (is (= (:gets-beer (chores-checker {:chores [:bathroom :kitchen :laundry :bins] :name "Charlotte"})) true)))
+
+  (testing "If a flatmate hasn't completed enough chores they don't get a beer"
+    (is (= (:gets-beer (chores-checker {:chores [:bathroom] :name "Charlotte"}) false))))
+
+
+  #_(testing "If a flatmate has hired a cleaner then they get three chores done for them:  bathroom, living room, kitchen ")
+
+  #_(testing "If a flatmate is ill they're exempt from doing a chore"
+    (is (= (:status (chores-checker {:name "Charlotte" :cleaner-hired true}) :exempted)))
+
 
     )
   )
-
-
-
-(comment
-  (deftest checks-chores-done
-    (testing "If a flatmate has completed a chore their chore status is marked as completed"
-      (is (= (chores-checker {:chore-type :vacuum :name "Charlotte"}) {:chore-type :vacuum :name "Charlotte" :chore-status :completed})))
-
-    (testing "If a flatmate hired a cleaner to complete their chore, their chore status is completed"
-      (is (= (chores-checker {:name "Charlotte" :cleaner-hired :vacuum}) {:chore-type :vacuum :name "Charlotte" :chore-status :completed})))
-
-
-    (testing "If a flatemate has got their other flatmate to complete their chore, their chore status is completed"
-      (is (= (chores-checker {:name "Charlotte" :flate-mate-completed :vacuum}) {:chore-type :vacuum :name "Charlotte" :chore-status :completed})))
-
-
-    (testing "If a flatemate was ill their chore status is marked as exempt"
-      (is (= (chores-checker {:name "Charlotte" :ill :flu}) {:name "Charlotte" :chore-status :exempt})))
-
-
-    (testing "If a flatmate has not completed a chore & hired no cleaner their chore status is marked as incomplete"
-      (is (= (chores-checker {:name "Felipe"}) {:name "Felipe" :chore-status :incomplete}))))
-
-  (deftest check-chores-outcome
-    (testing "If a flatmate has completed chores they receive a prize"
-      (is (= (outcome-checker {:chore-type :vacuum :name "Charlotte" :chore-status :completed}) (prn "Congrats you did your chores, as a prize you can choose the flat sunday night movie!"))))
-
-    (testing "If a flatmate has been ill they are exempt from completing their chores"
-      (is (= (outcome-checker {:name "Charlotte" :chore-status :exempt}) (prn "Sorry you were ill, get better soon"))))
-
-    (testing "If a flatmate has not done their chores"
-      (is (= (outcome-checker {:name "Felipe" :chore-status :incomplete}) (prn "Oh no you didn't do your chores, please buy your flatmates pizza for the flat sunday night movie!"))))))
