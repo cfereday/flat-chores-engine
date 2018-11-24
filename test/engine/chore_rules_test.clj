4e2f638cd52c2b7c833ac455ed721e99d978ddd1
Felipe Sere <felipesere@gmail.com>
2018-11-24 15:19:42 +0000

    Different style of rules

--- test/engine/chore_rules_test.clj
+++ test/engine/chore_rules_test.clj
@@ -4,6 +4,7 @@
             [clara.rules :refer :all]
             [clara.tools.inspect :refer :all]))
 
+(comment
 (deftest checks-chores-for-individual-flatmates
   (testing "If a flatmate has completed a chore their chore status is marked as completed"
     (let [session (-> (mk-session 'engine.chore-rules)
@@ -140,9 +141,106 @@
       (is (and
             (= (:?eligibility mapped-exempt-query) :no-movie-picking)
             (= (:?flatmate-name mapped-incomplete-query) "Charlotte"))))))
+)
+
+(deftest felipes-test-suite
+  (testing "That a sick flatname is automagically exempted"
+    (let [session (-> (mk-session 'engine.chore-rules)
+                    (insert (->FlatMateReport nil "Charlotte" nil nil :ear-infection))
+                    (fire-rules))
+          [{result :?exemption}] (query session exemptions-query?)]
+      (is (= result (->Exemption "Charlotte")))))
+
+  ;; this would need to be refactored into a blog that gets taken aparat, I guess?
+  (testing "doing 4 chores gives you beer"
+    (let [session (-> (mk-session 'engine.chore-rules)
+                    (insert (->FlatMateReport nil "Charlotte" nil nil nil))
+                    (insert (->Chore "Charlotte" :bathroom))
+                    (insert (->Chore "Charlotte" :kitchen))
+                    (insert (->Chore "Charlotte" :laundry))
+                    (insert (->Chore "Charlotte" :bins))
+                    (fire-rules))
+          [{result :?beer}] (query session beer?)
+          ;; _x (explain-activations session)]
+          ]
+      (is (= result (->Beer "Charlotte")))))
+
+  ;; this would need to be refactored into a blog that gets taken aparat, I guess?
+  (testing "getting the cleaners"
+    (let [session (-> (mk-session 'engine.chore-rules)
+                    (insert (->FlatMateReport nil "Charlotte" true nil nil))
+                    (insert (->Chore "Charlotte" :bins))
+                    (fire-rules))
+          [{result :?beer}] (query session beer?)
+          ;; _x (explain-activations session)]
+          ]
+      (is (= result (->Beer "Charlotte")))))
+
+  (testing "sadly being ill"
+    (let [session (-> (mk-session 'engine.chore-rules)
+                    (insert (->FlatMateReport nil "Charlotte" true nil :ear-infection))
+                    (insert (->Chore "Charlotte" :bins))
+                    (insert (->Chore "Charlotte" :kitchen))
+                    (insert (->Chore "Charlotte" :cooking))
+                    (insert (->Chore "Charlotte" :bathroom))
+                    (insert (->Chore "Charlotte" :balcony))
+                    (fire-rules))
+          result (query session beer?)
+          ;; _x (explain-activations session)]
+          ]
+      (is (empty? result ))))
+  
+
+;;  (defn beerable? [person]
+;;    (let [known-facts (derive-facts-from person)  ;; ---> derive-facts-from turns the person into a list of facts
+;;          session (create-session-with known-facts)] ;; ---> inserts said facts into Clara and runs fires the rules
+;;      (has-beer? session))) ;; ---> runs my query AND removes Clara'isms from it... aka ?foo should be gone!
+;;
+;;   {
+;;    :name "Charlotte"
+;;    :chores '(:bins :dishes :bedroom :kitchen)
+;;   }
+;; 
+;;   {
+;;    :name "Felipe"
+;;    :chores '(:kitchen)
+;;    :paid-cleaner true
+;;   }
+
+
+;;  (defn person-facts
+;;    [person]
+;;    (let [flatmate (make-flatmate person)
+;;          chores (map #(Chore. (:name person) %) (:chores person))]
+;;      '(flatmate chores)
+;;      ))
+;;
+;;  (defn derive-facts-from
+;;    [people]
+;;    (mapcat person-facts people))
+;;
+;;  (defn create-session-with 
+;;    [facts]
+;;    (let [session (mk-session 'engine.chore-rules)
+;;          with-facts (reduce insert session facts)]
+;;          (fire-rules with-facts)))
 
 
 
+  (testing "getting the cleaners"
+    (let [session (-> (mk-session 'engine.chore-rules)
+                    (insert (->FlatMateReport nil "Felipe" true nil nil))
+                    (insert (->Chore "Felipe" :bills))
+                    (insert (->FlatMateReport nil "Charlotte" nil nil nil))
+                    (insert (->Chore "Charlotte" :bins))
+                    (insert (->Chore "Charlotte" :dishes))
+                    (insert (->Chore "Charlotte" :bedroom))
+                    (insert (->Chore "Charlotte" :kitchen))
+                    (fire-rules))
+          [{result :?beer}] (query session beer?)
+          ;; _x (explain-activations session)]
+          ]
+      (is (= result (->Beer "Charlotte"))))))
 
 
 
